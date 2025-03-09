d3702fb51cd03e79848f5463d637e51b8972e6f5
lbbasilio <50463396+lbbasilio@users.noreply.github.com>
2025-03-09 00:21:30 -0300

    added arena_realloc

--- arena.h
+++ arena.h
@@ -4,15 +4,18 @@
 #define ARENA_H_
 
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
 	unsigned char* base;
 	unsigned char* head;
+	unsigned char* last;
 	size_t capacity;
 } Arena;
 
-Arena* arena_create(size_t capacity);
+Arena arena_create(size_t capacity);
 void* arena_alloc(Arena* arena, size_t bytes);
+void* arena_realloc(Arena* arena, void* ptr, size_t current, size_t target);
 unsigned char* arena_checkpoint(Arena* arena);
 void arena_rollback(Arena* arena, unsigned char* ptr);
 void arena_destroy(Arena* arena);
@@ -21,21 +24,18 @@ void arena_destroy(Arena* arena);
 
 #ifdef ARENA_IMPLEMENTATION
 
-Arena* arena_create(size_t capacity)
+Arena arena_create(size_t capacity)
 {
-	Arena* arena = malloc(sizeof(Arena));
-	if (arena == NULL)
-		return NULL;
-
-	arena->capacity = capacity;
-	arena->base = malloc(capacity);
-	arena->head = arena->base;
-	if (arena->base == NULL) {
-		free(arena);
-		return NULL;
-	}
-
-	return arena;
+	void* ptr = malloc(capacity);
+	if (ptr == NULL)
+		return (Arena) {0};
+	
+	return (Arena) {
+		.capacity = capacity,
+		.base = ptr,
+		.head = ptr,
+		.last = ptr
+	};
 }
 
 void* arena_alloc(Arena* arena, size_t bytes)
@@ -46,9 +46,33 @@ void* arena_alloc(Arena* arena, size_t bytes)
 	if (bytes > arena->capacity - (arena->head - arena->base))
 		return NULL;
 
-	void* ptr = arena->head;
+	arena->last = arena->head;
 	arena->head += bytes;
-	return ptr;
+	return arena->last;
+}
+
+void* arena_realloc(Arena* arena, void* ptr, size_t current, size_t target)
+{
+	if (arena == NULL || arena->base == NULL || target == 0)
+		return NULL;
+
+	size_t bytes_used = arena->head - arena->base;
+	if (ptr == arena->last) {
+		int size_diff = target - current;
+		if (size_diff < 0 || size_diff > arena->capacity - bytes_used)
+			return NULL;
+
+		arena->head += size_diff;
+		return ptr;
+	}
+
+	if (target > arena->capacity - (bytes_used))
+		return NULL;
+
+	arena->last = arena->head;
+	arena->head += target;
+	memcpy(arena->last, ptr, current);
+	return arena->last;
 }
 
 unsigned char* arena_checkpoint(Arena* arena)
@@ -64,11 +88,8 @@ void arena_rollback(Arena* arena, unsigned char* checkpoint)
 
 void arena_destroy(Arena* arena) 
 {
-	if (arena != NULL) {
-		if (arena->base != NULL)
-			free(arena->base);
-		free(arena);
-	}
+	if (arena != NULL && arena->base != NULL)
+		free(arena->base);
 }
 
 #endif
